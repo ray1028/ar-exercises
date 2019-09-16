b7ca32b62eeb9507d9063460aacbfa8d6a1fa986
ray1028 <rayjiang@Rays-MacBook-Pro.local>
2019-09-16 13:27:15 -0400

    generated store over 1m sales

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,18 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+store_surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+store_whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+store_yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |store|
+  puts "#{store.name}'s revenue is #{store.annual_revenue}'"
+end
+
+@women_stores = Store.where("womens_apparel= true and annual_revenue < 1000000")
+
+@women_stores.each do |store|
+  puts "The womens apparel with revenue less than 1M is #{store.name} with revenue #{store.annual_revenue}"
+end
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,6 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+puts "total revenue for all stores is #{Store.sum("annual_revenue")}"
+puts "average revenue for all stores is #{Store.average("annual_revenue")}"
+puts "total stores over 1M in sales is #{Store.where("annual_revenue >= 1000000").size()}"
